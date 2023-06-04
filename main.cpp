6e21eda4098c7238ba3817f950f7c1aa4c31ad0a
owenthcarey <owenthcarey@gmail.com>
2023-06-03 21:26:14 -0700

    Add .clang-tidy and update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,80 @@
 #include <wx/wx.h>
+#include <wx/file.h>
 
-class MyApp : public wxApp
-{
+class MyApp : public wxApp {
 public:
     virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame
-{
+class MyFrame : public wxFrame {
 public:
     MyFrame();
     wxTextCtrl *textCtrl;
+    void OnOpen(wxCommandEvent &event);
+    void OnSave(wxCommandEvent &event);
+    void OnExit(wxCommandEvent &event);
+
+private:
+wxDECLARE_EVENT_TABLE();
 };
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+                EVT_MENU(wxID_OPEN, MyFrame::OnOpen)
+                EVT_MENU(wxID_SAVE, MyFrame::OnSave)
+                EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+wxEND_EVENT_TABLE()
 
 IMPLEMENT_APP(MyApp)
 
-bool MyApp::OnInit()
-{
+bool MyApp::OnInit() {
     MyFrame *frame = new MyFrame();
     frame->Show(true);
     return true;
 }
 
-MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "wxWidgets Text Editor")
-{
-    textCtrl = new wxTextCtrl(this, -1, "", wxPoint(-1, -1), wxSize(-1, -1), wxTE_MULTILINE | wxTE_RICH2);
-    textCtrl->SetFont(wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "wxWidgets Text Editor",
+                             wxDefaultPosition, wxSize(800, 600)) {
+    textCtrl = new wxTextCtrl(this, -1, "", wxDefaultPosition, wxDefaultSize,
+                              wxTE_MULTILINE | wxTE_RICH2);
+    textCtrl->SetFont(wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL,
+                             wxFONTWEIGHT_NORMAL));
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(wxID_OPEN, "&Open\tCtrl-O", "Open a file");
+    menuFile->Append(wxID_SAVE, "&Save\tCtrl-S", "Save to a file");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    SetMenuBar(menuBar);
+}
+
+void MyFrame::OnOpen(wxCommandEvent &WXUNUSED(event)) {
+    wxFileDialog openFileDialog(this, _("Open file"), "", "",
+                                "Text files (*.txt)|*.txt",
+                                wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+    if (openFileDialog.ShowModal() == wxID_CANCEL) {
+        return;
+    }
+    wxFile file;
+    file.Open(openFileDialog.GetPath());
+    wxString str;
+    file.ReadAll(&str);
+    textCtrl->SetValue(str);
+}
+
+void MyFrame::OnSave(wxCommandEvent &WXUNUSED(event)) {
+    wxFileDialog saveFileDialog(this, _("Save file"), "", "",
+                                "Text files (*.txt)|*.txt",
+                                wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+    if (saveFileDialog.ShowModal() == wxID_CANCEL) {
+        return;
+    }
+    wxFile file;
+    file.Create(saveFileDialog.GetPath(), true);
+    file.Write(textCtrl->GetValue());
+    file.Close();
+}
+
+void MyFrame::OnExit(wxCommandEvent &WXUNUSED(event)) {
+    Close(true);
 }
